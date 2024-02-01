c698e24eeb622ddb27ba8b63d316acad31bfbd7e
Maxence Breuilles <40914400+MisterAzix@users.noreply.github.com>
2024-02-01 15:49:59 +0100

    feat: adding ec2 and bucket resources ✨

--- main.tf
+++ main.tf
@@ -5,11 +5,27 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  required_version = ">= 1.2.0"
 }
 
-# Configure the AWS Provider
 provider "aws" {
-  # region     = "us-west-2"
-  # access_key = "my-access-key"
-  # secret_key = "my-secret-key"
+  region = "eu-west-3"
+}
+
+resource "aws_instance" "hetic_aws_terraform_ec2" {
+  ami           = "ami-03eb7a5d774d59f31"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "hetic-aws-terraform-ec2"
+  }
+}
+
+resource "aws_s3_bucket" "hetic_aws_terraform_s3" {
+  bucket = "hetic-aws-terraform-s3"
+
+  tags = {
+    Name = "hetic-aws-terraform-s3"
+  }
 }
