3d4e9a6edee8eb8a6bf55118ddd7b8d706f2d799
Benoît Favrie <benoit14.favrie@gmail.com>
2024-02-01 12:01:24 +0100

    feat: setup terraform

--- main.tf
+++ main.tf
@@ -1,25 +1,15 @@
 terraform {
   required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 3.0.1"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
 
-provider "docker" {}
-
-resource "docker_image" "nginx" {
-  name         = "nginx"
-  keep_locally = false
-}
-
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
-  name  = "tutorial"
-
-  ports {
-    internal = 80
-    external = 8000
-  }
+# Configure the AWS Provider
+provider "aws" {
+  # region     = "us-west-2"
+  # access_key = "my-access-key"
+  # secret_key = "my-secret-key"
 }
